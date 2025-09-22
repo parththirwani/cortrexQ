94004f68522a11a6d73ccd63ad7c2c04e3117893
parththirwani <124462644+parththirwani@users.noreply.github.com>
2025-09-23 00:07:51 +0530

    next-conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['serpapi.com','lh3.googleusercontent.com','gold-legislative-tuna-190.mypinata.cloud','ui-avatars.com','encrypted-tbn3.gstatic.com'],
-      }
+  images: {
+    domains: [
+      "serpapi.com",
+      "lh3.googleusercontent.com",
+      "gold-legislative-tuna-190.mypinata.cloud",
+      "ui-avatars.com",
+      "encrypted-tbn0.gstatic.com",
+      "encrypted-tbn1.gstatic.com",
+      "encrypted-tbn2.gstatic.com",
+      "encrypted-tbn3.gstatic.com",
+    ],
+  },
 };
 
 export default nextConfig;
